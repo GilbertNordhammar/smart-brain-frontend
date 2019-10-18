989b40ccd06d869fd08dcf4c488b35c97e486bf4
oboking <boking@student.chalmers.se>
2019-10-18 15:26:32 +0200

    prettifying rank.js

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Rank = ({nameOfUser, entries}) => {
-    return (
-        <div className='tc white mt5'>
-            <div className='f3'>
-                {`${nameOfUser}, your current entry count is...`}
-            </div>
-            <div className='f1'>
-                {entries}
-            </div>
-        </div>
-    )
-}
+const Rank = ({ nameOfUser, entries }) => {
+  return (
+    <div className="tc white mt5">
+      <div className="f3">
+        {`${nameOfUser}, your current entry count is...`}
+      </div>
+      <div className="f1">{entries}</div>
+    </div>
+  );
+};
 
 export default Rank;
